8d0c32a13f0e835ec12c5c705d37d6509dd92537
RSPwFPGAs <RSPwFPGAs@github.com>
2020-07-26 22:28:31 +0800

    Need to parse 2.4.5 Descriptor Table

--- hw/src/qemu_hdl_cosim/axi_vip/dma_transaction_for_queue_notify.v
+++ hw/src/qemu_hdl_cosim/axi_vip/dma_transaction_for_queue_notify.v
@@ -52,23 +52,30 @@
   
       // read available ring flags+index(tail)
           debug_trace_rd(virt_queue_phy+(0+16*256)+0, data2);                  $display("1");
-        next_avail_idx  = data2[15:0]; 
+        next_avail_idx  = data2[31:16]; 
         num_avail_entry = next_avail_idx - curr_avail_idx;
       // read available ring entry
           debug_trace_rd(virt_queue_phy+(0+16*256)+4+curr_avail_idx*2, data2); $display("2"); // TODO: len?
         descr_idx = data2[15:0];
-      // read descriptor 2*16B
-          debug_trace_rd(virt_queue_phy+(0)+descr_idx*16+ 0, data2);              $display("3");
-          debug_trace_rd(virt_queue_phy+(0)+descr_idx*16+32, data2);              $display("4");
+      
+      // read descriptor num*16B
+          debug_trace_rd(virt_queue_phy+(0)+descr_idx*16, data2);              $display("3");
+      //accl:  dma_rd: @0x00000001384bc000(32) =0x00020002000005ee00000000bb421040,000100030000000a00000000bb18bb28  //2.4.5 The Virtqueue Descriptor Table
+
 
       // update current available index
         curr_avail_idx  = next_avail_idx;
      
+      //// read descriptor num*16B
+      //for (int i = 0; i < (num_avail_entry+0)* 16/32; i++) begin
+      //    debug_trace_rd(virt_queue_phy+(0)+descr_idx*16+i*32, data2);         $display("3, %d", i);
+      //end
+
       //// read used ring flags+index(tail)
-      //    debug_trace_rd(data1+(0+16*256+1*4096)+0, data2);
+      //    debug_trace_rd(virt_queue_phy+(0+16*256+1*4096)+0, data2);
       //// read 8 used ring id, from 4(head)
       //for (int i = 0; i < 8* 8/4; i++) begin
-      //    debug_trace_rd(data1+(1*16*256+1*4096)+4+i*4, data2);
+      //    debug_trace_rd(virt_queue_phy+(1*16*256+1*4096)+4+i*4, data2);
       //end
 
 
