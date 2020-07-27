a01f59063b26faf463c07f8f873d5ad815f16654
RSPwFPGAs <RSPwFPGAs@github.com>
2020-07-27 22:17:43 +0800

    Added full DMA transactions for VirtQueue operations.

--- hw/src/qemu_hdl_cosim/axi_vip/dma_transaction_for_queue_notify.v
+++ hw/src/qemu_hdl_cosim/axi_vip/dma_transaction_for_queue_notify.v
@@ -31,45 +31,80 @@
 //
 //    $fclose(file);
 
-  reg [63      :0] data1;
-  reg [8*4096-1:0] data2;
+  reg [63   :0] data1;
+  reg [256-1:0] data2;
 
   `define CSR_PATH test_top.DUT.shell_region_i.FIM.FIU.feature_ram.virtio_csr_0.inst
+  reg [15:0] virt_queue_sel = 0;
   reg [63:0] virt_queue_phy = 0;
   reg [15:0] curr_avail_idx = 0;
   reg [15:0] next_avail_idx = 0;
   reg [15:0] num_avail_entry = 0;
-  reg [15:0] descr_idx = 0;
+  reg [15:0] desc_idx = 0;
+  reg [16*8-1:0] desc_entry = 0;
+  reg [63:0] desc_entry_phy = 0;
+  reg [31:0] desc_entry_len = 0;
+  reg [15:0] desc_entry_flg = 0;
+  reg [15:0] desc_entry_nxt = 0;
+  reg [31:0] desc_chain_len= 0;
+
+  always begin
+    @(posedge `CSR_PATH.csr_drv_ok);
+    curr_avail_idx = 0;
+    next_avail_idx = 0;
+  end
 
   always begin
     @(posedge `CSR_PATH.csr_access_10B2);
     if (`CSR_PATH.csr_drv_ok) begin
 
-
-      repeat(2) @(posedge `CSR_PATH.clk); 
+      repeat(2) @(posedge `CSR_PATH.clk);
+      virt_queue_sel = `CSR_PATH.csr_reg_10B2;
       // get Virtqueue physical address
-      virt_queue_phy = {20'h00000, `CSR_PATH.csr_reg_08B4[`CSR_PATH.csr_reg_10B2][31:0], 12'h000};
+      virt_queue_phy = {20'h00000, `CSR_PATH.csr_reg_08B4[virt_queue_sel][31:0], 12'h000};
   
-      // read available ring flags+index(tail)
-          debug_trace_rd(virt_queue_phy+(0+16*256)+0, data2);                  $display("1");
-        next_avail_idx  = data2[31:16]; 
-        num_avail_entry = next_avail_idx - curr_avail_idx;
-      // read available ring entry
-          debug_trace_rd(virt_queue_phy+(0+16*256)+4+curr_avail_idx*2, data2); $display("2"); // TODO: len?
-        descr_idx = data2[15:0];
+
+      // read available ring flags+index(tail), 2B+2B
+      debug_trace_rd(virt_queue_phy+(0+16*256)+0, data2);                            $display("1");
+      next_avail_idx  = data2[31:16]; 
+      num_avail_entry = next_avail_idx - curr_avail_idx;
       
-      // read descriptor num*16B
-          debug_trace_rd(virt_queue_phy+(0)+descr_idx*16, data2);              $display("3");
-      //accl:  dma_rd: @0x00000001384bc000(32) =0x00020002000005ee00000000bb421040,000100030000000a00000000bb18bb28  //2.4.5 The Virtqueue Descriptor Table
+      for (int i = 0; i < num_avail_entry; i++) begin
+        // read available ring entry, num*2B
+        debug_trace_rd(virt_queue_phy+(0+16*256)+4+curr_avail_idx+i*2, data2);       $display("2, %d", i);
+        desc_idx = data2[15:0];
+      
+
+        desc_entry_flg = 16'h1; 
+        desc_entry_nxt = desc_idx;
+	desc_chain_len = 0;
+        while (desc_entry_flg[0]) begin
+          // read descriptor, num*chain_entry*16B
+          debug_trace_rd(virt_queue_phy+(0)+desc_entry_nxt*16, data2);              $display("3, %d", desc_entry_nxt);
+          //2.4.5 The Virtqueue Descriptor Table
+          desc_entry = (desc_entry_nxt[0] == 0)? data2[127:0]: data2[255:128];
+	  desc_entry_phy = desc_entry[ 63:  0];
+	  desc_entry_len = desc_entry[ 95: 64];
+          desc_entry_flg = desc_entry[111: 96];
+          desc_entry_nxt = desc_entry[127:112];
+	  desc_chain_len = desc_chain_len + desc_entry_len;
+	  // TODO: read/write buffer
+        end
+      
+
+        // write used ring entry, len+id, num*8B
+        data2 = {64'd0, 64'd0, 64'd0, {desc_chain_len, {16'd0, desc_idx}}};
+        debug_trace_wr(virt_queue_phy+(0+16*256+1*4096)+4+curr_avail_idx+i*8, data2);
+      end
+      
+      // write used ring flags+index(), 2B+2B
+      data2 = {64'd0, 64'd0, 64'd0, {32'd0, {next_avail_idx, 16'd0}}};
+      debug_trace_wr(virt_queue_phy+(0+16*256+1*4096)+0, data2);
 
 
       // update current available index
-        curr_avail_idx  = next_avail_idx;
+      curr_avail_idx  = next_avail_idx;
      
-      //// read descriptor num*16B
-      //for (int i = 0; i < (num_avail_entry+0)* 16/32; i++) begin
-      //    debug_trace_rd(virt_queue_phy+(0)+descr_idx*16+i*32, data2);         $display("3, %d", i);
-      //end
 
       //// read used ring flags+index(tail)
       //    debug_trace_rd(virt_queue_phy+(0+16*256+1*4096)+0, data2);
@@ -77,7 +112,6 @@
       //for (int i = 0; i < 8* 8/4; i++) begin
       //    debug_trace_rd(virt_queue_phy+(1*16*256+1*4096)+4+i*4, data2);
       //end
-
 
     end
   end
